d78609e7e7eaf4b42ef9882a14ddd0139f72f36e
vinicamposdev <viniciusmoliveirac@hotmail.com>
2022-01-24 14:52:52 -0300

    feat: add category model

---
+++ src/models/category.model.ts
@@ -0,0 +1,33 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model({settings: {strict: false}})
+export class Category extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: false,
+    required: true,
+  })
+  id: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  name: string;
+
+  @property({
+    type: 'string',
+  })
+  description?: string;
+
+  constructor(data?: Partial<Category>) {
+    super(data);
+  }
+}
+
+export interface CategoryRelations {
+  // describe navigational properties here
+}
+
+export type CategoryWithRelations = Category & CategoryRelations;
